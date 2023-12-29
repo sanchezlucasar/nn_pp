40084d3f04ea8047187f334b2229b3e116828fbc
sanchezlucasar <32554127+sanchezlucasar@users.noreply.github.com>
2023-12-29 11:57:29 -0300

    Update Providers.tsx

--- nextjs-prisma-auth/src/context/Providers.tsx
+++ nextjs-prisma-auth/src/context/Providers.tsx
@@ -1,15 +1,15 @@
 'use client'
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { SessionProvider, useSession } from "next-auth/react";
-import { useState } from 'react';
+import { SessionProvider } from "next-auth/react";
+
 
 export default function Providers({ children }: { children: React.ReactNode }) {
 
     const client = new QueryClient();
 
     return (
-        <SessionProvider >
+        <SessionProvider>
             <QueryClientProvider client={client}>
                 {children}
                 <ReactQueryDevtools initialIsOpen={false} />
